fd8ed6b37bcf7705d46eb7368d0e69504dbf3dcc
isaac gomes <93602614+isaac545454@users.noreply.github.com>
2022-12-10 22:25:21 -0300

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin={true} />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700;900&display=swap"
           rel="stylesheet"
